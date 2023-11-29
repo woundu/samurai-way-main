42d56060869aaa447bf86c23b4c04f29061fa02e
woundu <woundu@gmail.com>
2023-11-29 22:17:04 +0300

    Router

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,27 @@ import './App.css';
 import Header from './components/Header/Header';
 import Nav from './components/Nav/Nav';
 import Profile from './components/Profile/MyPosts/MyPosts';
+import Dialogs from './components/Dialogs/Dialogs';
+import MyPosts from './components/Profile/MyPosts/MyPosts';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="app-wrapper">
-      <Header/>
-      <Nav/>
-      <Profile/>
-    </div>
+    <BrowserRouter>
+
+      <div className="app-wrapper">
+        <Header />
+        <Nav />
+        <div className='app-wrapper-content'>
+
+        <Route path = "/profile" component ={MyPosts} />
+          <Route path="/dialogs" component={Dialogs} />
+          
+        </div>
+
+      </div>
+    </BrowserRouter>
+
   );
 }
 
